61e48356d87cb10a50bd0f2625de1a77c3a02773
agstover <agstover@gmail.com>
2021-03-29 16:31:47 -0500

    Updated footer with accurate email address

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div id='contact' className={'flex flex-row h-64 bg-gray-900 text-white justify-center items-center px-4'}>
-            <h1 className={'text-xm sm:text-2xl md:text-5xl font-bold'}>Kevin AT ArchitecturalInnovations DOT com</h1>
+            <h1 className={'text-xm sm:text-2xl md:text-5xl font-bold'}>Kevin AT aifabrication DOT net</h1>
         </div>
     )
 }
