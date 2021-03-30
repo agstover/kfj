e37b73aab9f12073bffd758e2c0ebf6cc163eea4
agstover <agstover@gmail.com>
2021-03-30 12:08:39 -0500

    Make footer use padding instead of height

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div id='contact' className={'flex flex-row h-64 bg-gray-900 text-white justify-center items-center px-1 sm:px-2 md:px-3'}>
+        <div id='contact' className={'flex flex-row bg-gray-900 text-white justify-center items-center px-1 sm:px-2 md:px-3 py-2 sm:py-3'}>
             <h1 className={'text-md sm:text-2xl md:text-3xl font-bold'}>Kevin AT aifabrication DOT net</h1>
         </div>
     )
