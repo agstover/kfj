cdf18fcdfaf279aec09d29c64cbbf7a5da05b7f6
agstover <agstover@gmail.com>
2021-03-30 12:12:44 -0500

    Increase footer y padding values

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div id='contact' className={'flex flex-row bg-gray-900 text-white justify-center items-center px-1 sm:px-2 md:px-3 py-2 sm:py-3'}>
+        <div id='contact' className={'flex flex-row bg-gray-900 text-white justify-center items-center px-1 sm:px-2 md:px-3 py-8 sm:py-12'}>
             <h1 className={'text-md sm:text-2xl md:text-3xl font-bold'}>Kevin AT aifabrication DOT net</h1>
         </div>
     )
